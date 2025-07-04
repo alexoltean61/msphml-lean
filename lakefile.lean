0c84d64987c0c60fa62fa2c42af92d891099b943
Alex Oltean <aoltean@mailbox.org>
2025-07-04 10:27:11 +0300

    lean: Bump toolchain version

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,6 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
 
-require aesop from git "https://github.com/leanprover-community/aesop"
-
 package «hybrid» where
   -- add package configuration options here
 
