1e2c05c5a406e552b11d224c22f26e0bbb0328d0
Alex Oltean <aoltean@mailbox.org>
2026-01-21 17:24:27 +0200

    SMC: Update signature

--- Hybrid/SMC/Axioms.lean
+++ Hybrid/SMC/Axioms.lean
@@ -1,4 +1,5 @@
 import Hybrid.SMC.Signature
+import Hybrid.Proof.Hilbert
 
 namespace SMC
 
@@ -121,17 +122,34 @@ def AInd (π : SMCForm SortCtrlStack)
          (γ : SMCForm SortConfig) : SMCForm SortConfig :=
         γ ⋀ [π*](γ ⟶ [π]γ) ←→ [π*] γ
 
-def Pgm (s i n : Symb.nominal SortVar)
-  : SMCForm SortStmt :=
-  s ::= 0;
-  i ::= 0;
-  while (++i <= n) do'
-    s ::= s + i
+inductive Axiom : {s : Sorts} → SMCForm s → Type
+  | CStmtAx {s1 s2}     : Axiom (CStmtAx s1 s2)
+  | Aint {vs mem n}     : Axiom (Aint vs mem n)
+  | Aid {vs mem x n}    : Axiom (Aid vs mem x n)
+  | App {vs mem x n}    : Axiom (App vs mem x n)
+  | DPlus {a1 a2}       : Axiom (DPlus a1 a2)
+  | Aplus {n1 n2 vs mem}: Axiom (Aplus n1 n2 vs mem)
+  | DLeq {a1 a2}        : Axiom (DLeq a1 a2)
+  | ALeq {n1 n2 vs mem} : Axiom (ALeq n1 n2 vs mem)
+  | ASkip {γ}           : Axiom (ASkip γ)
+  | DAsgn {x a}         : Axiom (DAsgn x a)
+  | AAsgn {n vs mem x}  : Axiom (AAsgn n vs mem x)
+  | DIf {bexp s1 s2}    : Axiom (DIf bexp s1 s2)
+  | DWhile {bexp s}     : Axiom (DWhile bexp s)
+  | AMem1 {x y h n m mem}: Axiom (AMem1 x y h n m mem)
+  | AMem2 {x n m mem}   : Axiom (AMem2 x n m mem)
+  | ACup {π π' γ}       : Axiom (ACup π π' γ)
+  | ASeq {π π' γ}       : Axiom (ASeq π π' γ)
+  | ATestTrue {v vs mem}: Axiom (ATestTrue v vs mem)
+  | ATestFalse {v v' vs mem}: Axiom (ATestFalse v v' vs mem)
+  | AStar {π γ}         : Axiom (AStar π γ)
+  | AInd {π γ}          : Axiom (AInd π γ)
 
-def PgmCorrect (vs : SMCForm SortValStack)
-        (mem : SMCForm SortMem)
-        (s i n : Symb.nominal SortVar)
-        (vn : ℕ) : SMCForm SortConfig :=
-    ⟨vs, set(mem, n, vn)⟩ ⟶ [c(Pgm s i n)] ⟨vs, set(set(set(mem, n, vn), n, vn * (vn.add 1).div2), i, vn.add 1)⟩
+-- The set of axioms for SMC is that of formulas φ for which a term
+-- Axiom φ exists, for all sorts s:
+def SMCΛ : AxiomSet Symb := λ s => { φ | Nonempty (Axiom φ) }
+
+-- The Hilbert proof system for SMC:
+def SMCProof := Proof SMCΛ
 
 end SMC
