f27cf5ec8b683e08a32ddd5d0a763f59db43568c
Alex Oltean <aoltean@mailbox.org>
2026-01-27 12:35:49 +0200

    examples: Migrate SMC to new BNF format

--- Hybrid/Examples/SMC/Axioms.lean
+++ Hybrid/Examples/SMC/Axioms.lean
@@ -1,135 +1,129 @@
 import Hybrid.Examples.SMC.Signature
 import Hybrid.Proof.Hilbert
+import Hybrid.Proof.Proofs
 
 namespace SMC
 
--- TODO: check if necessary!
-def CSeq (c1 c2 : SMCForm SortCtrlStack)
-         (φ : SMCForm SortConfig)
-  : SMCForm SortConfig :=
-    ([c1][c2] φ) ⟶ [c1 ; c2] φ
-
 -- c(s1 ; s2) ↔ c(s1) ; c(s2)
-def CStmtAx (s1 s2 : SMCForm SortStmt)
-  : SMCForm SortCtrlStack :=
+def CStmtAx (s1 s2 : SMCForm Stmt)
+  : SMCForm CtrlStack :=
     c(s1 ; s2) ←→ c(s1) ; c(s2)
 
 -- ⟨vs, mem⟩ → [c(n)] ⟨n ⬝ vs, mem⟩, where n is an integer
-def Aint (vs : SMCForm SortValStack)
-         (mem : SMCForm SortMem)
+def Aint (vs : SMCForm ValStack)
+         (mem : SMCForm Mem)
          (n : ℕ)
-    : SMCForm SortConfig :=
+    : SMCForm Config :=
     ⟨vs, mem⟩ ⟶ [c(n)] ⟨n ⬝ vs, mem⟩
 
 -- ⟨vs, set(mem, x, n)⟩ → [c(x)] ⟨n ⬝ vs, set(mem, x, n)⟩
-def Aid (vs : SMCForm SortValStack)
-        (mem : SMCForm SortMem)
-        (x : SMCForm SortVar)
+def Aid (vs : SMCForm ValStack)
+        (mem : SMCForm Mem)
+        (x : SMCForm Var)
         (n : ℕ)
-    : SMCForm SortConfig :=
+    : SMCForm Config :=
     ⟨vs, set(mem, x, n)⟩ ⟶ [c(x)] ⟨n ⬝ vs, set(mem, x, n)⟩
 
 -- ⟨vs, set(mem, x, n)⟩ → [c(++x)] ⟨(n+1) ⬝ vs, set(mem, x, n+1)⟩
-def App  (vs : SMCForm SortValStack)
-         (mem : SMCForm SortMem)
-         (x : SMCForm SortVar)
+def App  (vs : SMCForm ValStack)
+         (mem : SMCForm Mem)
+         (x : SMCForm Var)
          (n : ℕ)
-  : SMCForm SortConfig :=
+  : SMCForm Config :=
     ⟨vs, set(mem, x, n)⟩ ⟶ [c(++x)] ⟨↑(n.add 1) ⬝ vs, set(mem, x, ↑(n.add 1))⟩
 
 -- c(a1 + a2) ↔ c(a1) ; c(a2) ; plus
-def DPlus (a1 a2 : SMCForm SortAExp) : SMCForm SortCtrlStack :=
+def DPlus (a1 a2 : SMCForm AExp) : SMCForm CtrlStack :=
     c(a1 + a2) ←→ c(a1) ; c(a2) ; plus
 
 -- ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [plus] ⟨n1 + n2 ⬝ vs, mem⟩
 def Aplus (n1 n2 : ℕ)
-          (vs : SMCForm SortValStack)
-          (mem : SMCForm SortMem)
-    : SMCForm SortConfig :=
+          (vs : SMCForm ValStack)
+          (mem : SMCForm Mem)
+    : SMCForm Config :=
       ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [plus] ⟨↑(n1.add n2) ⬝ vs, mem⟩
 
 -- c(a1 <= a2) ↔ c(a1) ; c(a2) ; leq
-def DLeq (a1 a2 : SMCForm SortAExp) : SMCForm SortCtrlStack :=
+def DLeq (a1 a2 : SMCForm AExp) : SMCForm CtrlStack :=
     c(a1 <= a2) ←→ c(a1) ; c(a2) ; leq
 
 -- ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [leq] ⟨↑(n1 ≤ n2), mem⟩
 def ALeq (n1 n2 : ℕ)
-          (vs : SMCForm SortValStack)
-          (mem : SMCForm SortMem)
-    : SMCForm SortConfig :=
+          (vs : SMCForm ValStack)
+          (mem : SMCForm Mem)
+    : SMCForm Config :=
       ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [leq] ⟨↑(n1.ble n2) ⬝ vs, mem⟩
 
 -- γ ⟶ [skip] γ
-def ASkip (γ : SMCForm SortConfig): SMCForm SortConfig :=
-      γ ⟶ [skip] γ
+def ASkip (γ : SMCForm Config): SMCForm Config :=
+      γ ⟶ [c(skip)] γ
 
 -- DAsgn c(x := a) ↔ c(a) ; asgn(x)
-def DAsgn (x : SMCForm SortVar)
-          (a : SMCForm SortAExp) : SMCForm SortCtrlStack :=
+def DAsgn (x : SMCForm Var)
+          (a : SMCForm AExp) : SMCForm CtrlStack :=
       c(x ::= a) ←→ c(a) ; asgn(x)
 
 -- AAsgn ⟨n ⬝ vs, mem) ⟶ [asgn(x)] ⟨vs, set(mem, x, n)⟩
-def AAsgn (n : SMCForm SortVal)
-          (vs : SMCForm SortValStack)
-          (mem : SMCForm SortMem)
-          (x : SMCForm SortVar): SMCForm SortConfig :=
+def AAsgn (n : SMCForm Val)
+          (vs : SMCForm ValStack)
+          (mem : SMCForm Mem)
+          (x : SMCForm Var): SMCForm Config :=
       ⟨n ⬝ vs, mem⟩ ⟶ [asgn(x)] ⟨vs, set(mem, x, n)⟩
 
 -- DIf
 --   c(if bexp then s1 else s2) ↔ c(bexp) ; ( (true? ; c(s1)) ∪ (false? ; c(s2)) )
-def DIf (bexp : SMCForm SortBExp)
-          (s1 s2 : SMCForm SortStmt) : SMCForm SortCtrlStack :=
+def DIf (bexp : SMCForm BExp)
+          (s1 s2 : SMCForm Stmt) : SMCForm CtrlStack :=
       c(if bexp then s1 else s2 endif) ←→ c(bexp) ; ((true ? ; c(s1)) ∪ (false ? ; c(s2)))
 
 -- DWhile
 --   c(while bexp do s) ↔ c(bexp) ; ( (true?) ; c(s) ; c(bexp) )* ; false?
-def DWhile (bexp : SMCForm SortBExp)
-          (s : SMCForm SortStmt) : SMCForm SortCtrlStack :=
+def DWhile (bexp : SMCForm BExp)
+          (s : SMCForm Stmt) : SMCForm CtrlStack :=
     c(while bexp do' s ) ←→ c(bexp) ; (true ? ; c(s) ; c(bexp))* ; false ?
 
 -- Memory consistency axioms:
 
-def AMem1 (x y : SMCForm SortVar)
+def AMem1 (x y : SMCForm Var)
           (h : x ≠ y)
-          (n m : SMCForm SortVal)
-          (mem : SMCForm SortMem) : SMCForm SortMem :=
+          (n m : SMCForm Val)
+          (mem : SMCForm Mem) : SMCForm Mem :=
         set(set(mem, x, n), y, m) ←→ set(set(mem, y, m), x, n)
 
-def AMem2 (x : SMCForm SortVar)
-          (n m : SMCForm SortVal)
-          (mem : SMCForm SortMem) : SMCForm SortMem :=
+def AMem2 (x : SMCForm Var)
+          (n m : SMCForm Val)
+          (mem : SMCForm Mem) : SMCForm Mem :=
         set(set(mem, x, n), x, m) ←→ set(mem, x, m)
 
 -- PDL-inspired axioms:
-def ACup (π π' : SMCForm SortCtrlStack)
-         (γ : SMCForm SortConfig) : SMCForm SortConfig :=
+def ACup (π π' : SMCForm CtrlStack)
+         (γ : SMCForm Config) : SMCForm Config :=
         [π ∪ π'] γ ←→ [π] γ ⋀ [π'] γ
 
-def ASeq (π π' : SMCForm SortCtrlStack)
-         (γ : SMCForm SortConfig) : SMCForm SortConfig :=
-        [π ; π'] γ ←→ [π][π'] γ
+def ASeq (π π' : SMCForm CtrlStack)
+         (γ : SMCForm Config) : SMCForm Config :=
+        ([π ; π'] γ) ←→ [π][π'] γ  -- FIXME: binding of implications / equivs
 
-def ATestTrue (v : SMCForm SortVal)
-         (vs : SMCForm SortValStack)
-         (mem : SMCForm SortMem) : SMCForm SortConfig :=
+def ATestTrue (v : SMCForm Val)
+         (vs : SMCForm ValStack)
+         (mem : SMCForm Mem) : SMCForm Config :=
         ⟨v ⬝ vs, mem⟩ ←→ [v ?] ⟨vs, mem⟩
 
-def ATestFalse (v : Symb.nominal SortVal)
-         (v' : SMCForm SortVal)
-         (vs : SMCForm SortValStack)
-         (mem : SMCForm SortMem) : SMCForm SortConfig :=
+def ATestFalse (v : CtNoms Val)
+         (v' : SMCForm Val)
+         (vs : SMCForm ValStack)
+         (mem : SMCForm Mem) : SMCForm Config :=
         ⟨v ⬝ vs, mem⟩ ⋀ ℋ@ v (∼v') ←→ [v' ?] ℋ⊥
 
-def AStar (π : SMCForm SortCtrlStack)
-         (γ : SMCForm SortConfig) : SMCForm SortConfig :=
+def AStar (π : SMCForm CtrlStack)
+         (γ : SMCForm Config) : SMCForm Config :=
         [π*] γ ←→ γ ⋀ [π][π*] γ
 
-def AInd (π : SMCForm SortCtrlStack)
-         (γ : SMCForm SortConfig) : SMCForm SortConfig :=
+def AInd (π : SMCForm CtrlStack)
+         (γ : SMCForm Config) : SMCForm Config :=
         γ ⋀ [π*](γ ⟶ [π]γ) ←→ [π*] γ
 
 inductive Axiom : {s : Sorts} → SMCForm s → Type
-  | CSeq {c1 c2 φ}      : Axiom (CSeq c1 c2 φ)
   | CStmtAx {s1 s2}     : Axiom (CStmtAx s1 s2)
   | Aint {vs mem n}     : Axiom (Aint vs mem n)
   | Aid {vs mem x n}    : Axiom (Aid vs mem x n)
@@ -153,12 +147,18 @@ inductive Axiom : {s : Sorts} → SMCForm s → Type
   | AInd {π γ}          : Axiom (AInd π γ)
 
 -- The set of axioms for SMC is that of formulas φ for which a term
--- Axiom φ exists, for all sorts s:
-@[simp] def SMCΛ : AxiomSet Symb := λ s => { φ | Nonempty (Axiom φ) }
+-- Axiom φ exists, for all s s:
+@[simp] def SMCΛ : AxiomSet SMC := λ s => { φ | Nonempty (Axiom φ) }
 
 -- The Hilbert proof system for SMC:
 def SMCProof := Proof SMCΛ
 
-def cseqAx : SMCProof _ (CSeq c1 c2 φ) := .ax ⟨_, .intro .CSeq⟩
+def aseqL : SMCProof _
+  (([π ; π'] γ) ⟶ [π][π'] γ) := -- FIXME: binding of implications!
+  .mp .conj_elimL_proof (.ax ⟨_, .intro .ASeq⟩)
+
+def aseqR : SMCProof _
+  (([π][π'] γ) ⟶ [π ; π'] γ) := -- FIXME: binding of implications!
+  .mp .conj_elimR_proof (.ax ⟨_, .intro .ASeq⟩)
 
 end SMC
--- Hybrid/Examples/SMC/Signature.lean
+++ Hybrid/Examples/SMC/Signature.lean
@@ -1,241 +1,85 @@
-import Hybrid.Language.Signature
+import Hybrid.BNF
 import Hybrid.Language.Form
 
-namespace SMC
+hybrid_def SMC :=
+    sort Nat  ::= builtin Nat
+    sort Bool ::= builtin Bool | "_==_"(Nat, Nat) | "_<=_"(Nat, Nat)
 
--- Define sorts:
-abbrev Sorts : Set String := {
-  "Nat", "Bool", "Var", "AExp",
-  "BExp", "Stmt", "Val", "ValStack",
-  "Mem", "CtrlStack", "Config"
-}
+    sort Var  ::= builtin String
+    sort AExp ::= subsort Nat | subsort Var
+    sort AExp ::= "_+_"(AExp, AExp) | "++"(Var)
+    sort BExp ::= "_<=_"(AExp, AExp)                [Leq]
+    sort Stmt ::= skip
+                | "_:=_"(Var, AExp)
+                | "if_then_else_"(BExp, Stmt, Stmt) [IteStmt]
+                | "while_do_"(BExp, Stmt)           [WhileStmt]
+                | "_;_"(Stmt, Stmt)                 [SeqStmt]
 
-abbrev SortNat  : Sorts.Elem := ⟨"Nat",  by aesop⟩
-abbrev SortBool : Sorts.Elem := ⟨"Bool", by aesop⟩
-abbrev SortVar  : Sorts.Elem := ⟨"Var",  by aesop⟩
-abbrev SortAExp : Sorts.Elem := ⟨"AExp", by aesop⟩
-abbrev SortBExp : Sorts.Elem := ⟨"BExp", by aesop⟩
-abbrev SortStmt : Sorts.Elem := ⟨"Stmt", by aesop⟩
-abbrev SortVal  : Sorts.Elem := ⟨"Val",  by aesop⟩
-abbrev SortValStack : Sorts.Elem := ⟨"ValStack",   by aesop⟩
-abbrev SortMem  : Sorts.Elem := ⟨"Mem",  by aesop⟩
-abbrev SortCtrlStack : Sorts.Elem := ⟨"CtrlStack", by aesop⟩
-abbrev SortConfig : Sorts.Elem := ⟨"Config", by aesop⟩
+    sort Val ::= subsort Nat | subsort Bool
+    sort ValStack ::= nil
+                | "_·_"(Val, ValStack)        [consValStack]
+    sort Mem ::= empty | "set"(Mem, Var, Val)
+    sort CtrlStack ::= "c"(AExp)              [cAExp]
+                | "c"(BExp)                   [cBExp]
+                | "c"(Stmt)                   [cStmt]
+                | "asgn"(Var)
+                | plus | leq
+                | "_?"(Val)                   [PDLTest]
+                | "_∪_"(CtrlStack, CtrlStack) [PDLUnion]
+                | "_;_"(CtrlStack, CtrlStack) [PDLSeq]
+                | "*"(CtrlStack)              [PDLStar]
+    sort Config ::= "<_,_>"(ValStack, Mem)    [mkConfig]
+                | "[_]_"(CtrlStack, Config)   [PDLOp]
 
--- Define operators:
-@[simp]
-def Op : List Sorts → Sorts → Set String := λ domain range =>
-  if domain = [SortNat, SortNat] ∧ range = SortBool then
-    { "==", "<=" }
-  else if domain = [SortNat] ∧ range = SortAExp then
-    { "nat2AExp" }
-  else if domain = [SortAExp, SortAExp] ∧ range = SortAExp then
-    { "+" }
-  else if domain = [SortVar] ∧ range = SortAExp then
-    { "++", "var2AExp" }
-  else if domain = [SortAExp, SortAExp] ∧ range = SortBExp then
-    { "<=" }
-  else if domain = [SortVar, SortAExp] ∧ range = SortStmt then
-    { ":=" }
-  else if domain = [SortBExp, SortStmt, SortStmt] ∧ range = SortStmt then
-    { "ite" }
-  else if domain = [SortStmt, SortStmt] ∧ range = SortStmt then
-    { ";" }
-  else if domain = [SortBExp, SortStmt] ∧ range = SortStmt then
-    { "while" }
-  else if domain = [SortNat] ∧ range = SortVal then
-    { "nat2Val" }
-  else if domain = [SortBool] ∧ range = SortVal then
-    { "bool2Val" }
-  else if domain = [SortVal, SortValStack] ∧ range = SortValStack then
-    { "·" }
-  else if domain = [SortMem, SortVar, SortVal] ∧ range = SortMem then
-    { "set" }
-  else if domain = [SortAExp] ∧ range = SortCtrlStack then
-    { "cAExp" }
-  else if domain = [SortBExp] ∧ range = SortCtrlStack then
-    { "cBExp" }
-  else if domain = [SortStmt] ∧ range = SortCtrlStack then
-    { "cStmt" }
-  else if domain = [SortVar] ∧ range = SortCtrlStack then
-    { "asgn" }
-  else if domain = [SortVal] ∧ range = SortCtrlStack then
-    { "?" }
-  else if domain = [SortCtrlStack, SortCtrlStack] ∧ range = SortCtrlStack then
-    { ";", "∪" } -- PDL-inspired symbols
-  else if domain = [SortCtrlStack] ∧ range = SortCtrlStack then
-    { "*" } -- PDL-inspired symbol
-  else if domain = [SortValStack, SortMem] ∧ range = SortConfig then
-    { "< >" } -- Configuration symbol; should find something better
-  else if domain = [SortCtrlStack, SortConfig] ∧ range = SortConfig then
-    { "[ ]" } -- PDL-inspired symbol; should find something better
-  else { }
+abbrev SMCFormL := FormL SMC
+abbrev SMCForm  := Form SMC
 
-@[simp]
-def CtNom : Sorts → Set String := λ s =>
-  if s = SortNat then
-    -- All natural numbers, as strings:
-    { s | ∃ n : ℕ, s = n.repr }
-    -- Alternative: try s.isNat
-  else if s = SortBool then
-    { "true", "false" }
-  else if s = SortStmt then
-    { "skip" }
-  else if s = SortValStack then
-    { "nil" }
-  else if s = SortMem then
-    { "empty" }
-  else if s = SortCtrlStack then
-    { "plus", "leq", "skip" }
-  else { "n" }
-
-@[simp]
-def Sig : Signature String where
-  S   := Sorts
-  «Σ» := Op
-  N   := CtNom
-
-  sortsCtbl := sorry -- explicit encoding of sorts to ℕ; todo
-  opsCtbl   := sorry -- explicit encoding of ops to ℕ; todo
-  nomCtbl   := sorry -- explicit encoding of ctNoms to ℕ; todo
-
-  sNonEmpty := ⟨SortNat⟩
-  nNonEmpty := sorry -- constant nominals inhabited; todo
-
-@[simp]
-def Symb : Symbols String where
-  signature := Sig
-  prop := λ _ => { "p" }
-  nom  := λ s =>
-      if s = SortVar then
-      -- Temporarily accept all strings as valid
-      -- variable names. Ideally this should be a more restricted
-      -- grammar:
-      { s | s = s }
-    else { }
-  svar := λ _ => { s | ∃ n : ℕ, s = "x" ++ n.repr }
-  propCtbl := sorry  -- explicit encoding of props to ℕ; todo
-  nomCtbl  := sorry  -- explicit encoding of noms to ℕ; todo
-  svarCtbl := sorry  -- explicit encoding of vars to ℕ; todo
-  nomExt   := sorry  -- extension set of nominals; needed for completenes; todo
-  hExtInf  := sorry  -- nomExt is countable; needed for completeness; todo
-  hExtDisj := sorry  -- nomExt can be partitioned in two countable sets; needed for completeness; todo
-
-  -- == :: (SortNat, SortNat) -> SortBool
-def EqNat : (Symb.signature.«Σ» [SortNat, SortNat] SortBool).Elem := ⟨"==", by aesop⟩
--- <= :: (SortNat, SortNat) -> SortBool
-def LEqNat : (Symb.signature.«Σ» [SortNat, SortNat] SortBool).Elem := ⟨"<=", by aesop⟩
-
--- nat2AExp :: SortNat -> SortAExp
-def nat2AExp : (Symb.signature.«Σ» [SortNat] SortAExp).Elem := ⟨"nat2AExp", by aesop⟩
--- var2AExp :: SortVar -> SortAExp
-def var2AExp : (Symb.signature.«Σ» [SortVar] SortAExp).Elem := ⟨"var2AExp", by aesop⟩
-
--- + :: (SortAExp, SortAExp) -> SortAExp
-def PlusNat : (Symb.signature.«Σ» [SortAExp, SortAExp] SortAExp).Elem := ⟨"+", by aesop⟩
--- ++ :: SortVar -> SortAExp
-def PlusPlusVar : (Symb.signature.«Σ» [SortVar] SortAExp).Elem := ⟨"++", by aesop⟩
-
--- <= :: (SortAExp, SortAExp) -> SortBExp
--- TODO: There is also a <= strictly between naturals, returning strictly booleans
--- Why both?
-def LEqAExp : (Symb.signature.«Σ» [SortAExp, SortAExp] SortBExp).Elem := ⟨"<=", by aesop⟩
-
--- skip :: SortStmt
--- skip is a nominal
-
--- Paper states:
---  Stmt ::= x := AExp
--- Shouldn't this rather be?
---  Stmt ::= Var := AExp
---
--- := :: (SortVar, SortAExp) -> SortStmt
-def AsgnStmt : (Symb.signature.«Σ» [SortVar, SortAExp] SortStmt).Elem := ⟨":=", by aesop⟩
--- ite :: (SortBExp, SortStmt, SortStmt) -> SortStmt
-def IteStmt : (Symb.signature.«Σ» [SortBExp, SortStmt, SortStmt] SortStmt).Elem := ⟨"ite", by aesop⟩
--- seq :: (SortStmt, SortStmt) -> SortStmt
-def SeqStmt : (Symb.signature.«Σ» [SortStmt, SortStmt] SortStmt).Elem := ⟨";", by aesop⟩
--- while :: (SortBExp, SortStmt) -> SortStmt
-def WhileStmt : (Symb.signature.«Σ» [SortBExp, SortStmt] SortStmt).Elem := ⟨"while", by aesop⟩
-
--- nat2Val :: SortNat -> SortVal
-def nat2Val : (Symb.signature.«Σ» [SortNat] SortVal).Elem := ⟨"nat2Val", by aesop⟩
--- bool2Val :: SortBool -> SortVal
-def bool2Val : (Symb.signature.«Σ» [SortBool] SortVal).Elem := ⟨"bool2Val", by aesop⟩
-
--- . :: (Val, ValStack) -> ValStack
-def consValStack : (Symb.signature.«Σ» [SortVal, SortValStack] SortValStack).Elem := ⟨"·", by aesop⟩
--- set :: (Mem, Var, AExp) -> Mem
-def setMem : (Symb.signature.«Σ» [SortMem, SortVar, SortVal] SortMem).Elem := ⟨"set", by aesop⟩
-
--- c :: AExp -> CtrlStack
-def cAExp : (Symb.signature.«Σ» [SortAExp] SortCtrlStack).Elem := ⟨"cAExp", by aesop⟩
--- c :: BExp -> CtrlStack
-def cBExp : (Symb.signature.«Σ» [SortBExp] SortCtrlStack).Elem := ⟨"cBExp", by aesop⟩
-
--- c :: Stmt -> CtrlStack
-def cStmt : (Symb.signature.«Σ» [SortStmt] SortCtrlStack).Elem := ⟨"cStmt", by aesop⟩
--- asgn :: Var -> CtrlStack
-def AsgnCtrlStack : (Symb.signature.«Σ» [SortVar] SortCtrlStack).Elem := ⟨"asgn", by aesop⟩
-
--- < > :: (ValStack, Mem) -> Config
-def mkConfig : (Symb.signature.«Σ» [SortValStack, SortMem] SortConfig).Elem := ⟨"< >", by aesop⟩
-
--- PDL :: (CtrlStack, Config) -> Config
--- Note!: [α] φ = ¬ ⟨α⟩ ¬ φ
-def PDLOp : (Symb.signature.«Σ» [SortCtrlStack, SortConfig] SortConfig).Elem := ⟨"[ ]", by aesop⟩
-
--- ? ::    Val -> CtrlStack
-def PDLTest : (Symb.signature.«Σ» [SortVal] SortCtrlStack).Elem := ⟨"?", by aesop⟩
--- seq :: (CtrlStack, CtrlStack) -> CtrlStack
-def PDLSeq : (Symb.signature.«Σ» [SortCtrlStack, SortCtrlStack] SortCtrlStack).Elem := ⟨";", by aesop⟩
-
--- PDLUnion :: (CtrlStack, CtrlStack) -> CtrlStack
-def PDLUnion : (Symb.signature.«Σ» [SortCtrlStack, SortCtrlStack] SortCtrlStack).Elem := ⟨"∪", by aesop⟩
-
--- PDLStar :: (CtrlStack) -> CtrlStack
-def PDLStar : (Symb.signature.«Σ» [SortCtrlStack] SortCtrlStack).Elem := ⟨"*", by aesop⟩
-
-abbrev SMCFormL := FormL Symb
-abbrev SMCForm  := Form Symb
-
-instance : Coe ℕ (SMCForm SortNat) where
-  coe := λ n => ℋNom (.ctNom ⟨n.repr, ⟨n, rfl⟩⟩)
-instance : Coe Bool (SMCForm SortBool) where
+instance : Coe ℕ (SMCForm SMC.Nat) where
+  coe := λ n => ℋNom (.ctNom ⟨toString n, ⟨n, rfl⟩⟩)
+instance : Coe Bool (SMCForm SMC.Bool) where
   coe := λ b =>
-    match b with
-    | true => ℋNom (.ctNom  ⟨"true", by simp [Symb, Sig, CtNom, SortBool, SortNat]⟩)
-    | false => ℋNom (.ctNom ⟨"false", by simp [Symb, Sig, CtNom, SortBool, SortNat]⟩)
-instance : Coe (SMCForm SortNat) (SMCForm SortAExp) where
+    ℋNom (.ctNom  ⟨toString b, by cases b <;> simp⟩)
+
+namespace SMC
+/-
+  Subsort operators probably also require some specific axioms
+  (to the extent of, e.g., nat2AEXp's accessibility relation
+  is an injective function)
+-/
+instance : Coe (SMCForm Nat) (SMCForm AExp) where
   coe := λ n => ℋ⟨nat2AExp⟩ n
-instance : Coe (SMCForm SortNat) (SMCForm SortVal) where
+instance : Coe (SMCForm Nat) (SMCForm Val) where
   coe := λ n => ℋ⟨nat2Val⟩ n
-instance : OfNat (SMCForm SortAExp) n where
+instance : OfNat (SMCForm AExp) n where
   ofNat := ℋ⟨nat2AExp⟩ n
-instance : Coe (SMCForm SortVar) (SMCForm SortAExp) where
+instance : Coe (SMCForm Var) (SMCForm AExp) where
   coe := λ v => ℋ⟨var2AExp⟩ v
-instance : Coe (SMCForm SortBool) (SMCForm SortVal) where
+instance : Coe (SMCForm Bool) (SMCForm Val) where
   coe := λ b => ℋ⟨bool2Val⟩ b
+instance : Coe (CtNoms s) (SMCForm s) where
+  coe := λ c => FormL.nom (.ctNom c)
 
 class Evaluable (α : Type u) where
-  ctrlStackEval : α → SMCForm SortCtrlStack
+  ctrlStackEval : α → SMCForm CtrlStack
 
-instance : Evaluable (SMCForm SortAExp) where
+instance : Evaluable (SMCForm AExp) where
   ctrlStackEval := FormL.appl cAExp
-instance : Evaluable (SMCForm SortBExp) where
+instance : Evaluable (SMCForm BExp) where
   ctrlStackEval := FormL.appl cBExp
-instance : Evaluable (SMCForm SortStmt) where
+instance : Evaluable (SMCForm Stmt) where
   ctrlStackEval := FormL.appl cStmt
+instance : Evaluable (CtNoms Stmt) where
+  ctrlStackEval c := FormL.appl cStmt c
 instance : Evaluable ℕ where
   ctrlStackEval := λ n => FormL.appl cAExp n
-instance : Evaluable (SMCForm SortVar) where
+instance : Evaluable (SMCForm Var) where
   ctrlStackEval := λ x => FormL.appl cAExp x
 
 class HasSeq (α : Type u) where
   seq : α → α → α
-instance : HasSeq (SMCForm SortStmt) where
+instance : HasSeq (SMCForm Stmt) where
   seq := λ s1 s2 => FormL.appl SeqStmt (FormL.cons s1 s2)
-instance : HasSeq (SMCForm SortCtrlStack) where
+instance : HasSeq (SMCForm CtrlStack) where
   seq := λ c1 c2 => FormL.appl PDLSeq (FormL.cons c1 c2)
 
 notation:100 s1:99 ";" s2:100 => HasSeq.seq s1 s2
@@ -243,26 +87,17 @@ notation:100 "c" "(" φ:100 ")" => Evaluable.ctrlStackEval φ
 notation:100 "⟨" vs ", " mem "⟩" => ℋ⟨mkConfig⟩ (vs, mem)
 -- Note!: [α] φ = ¬ ⟨α⟩ ¬ φ
 notation:100 "[" ctrl "]" config => ℋ⟨PDLOp⟩ᵈ (∼ctrl, config)
-notation:100 "asgn" "(" x ")" => ℋ⟨AsgnCtrlStack⟩ x
-notation:100 x:101 "::=" a:101 => ℋ⟨AsgnStmt⟩ (x, a)
+notation:100 "asgn" "(" x ")" => ℋ⟨SMC.asgnVar_CtrlStack⟩ x
+notation:100 x:101 "::=" a:101 => ℋ⟨SMC.«_:=_Var_AExp_Stmt»⟩ (x, a)
 notation:100 "if" bexp "then" s1 "else" s2 "endif" => ℋ⟨IteStmt⟩ (bexp, s1, s2)
 notation:100 "while" bexp "do'" s => ℋ⟨WhileStmt⟩ (bexp, s)
 notation:100 c1 "∪" c2 => ℋ⟨PDLUnion⟩ (c1, c2)
 notation:100 c1"*" => ℋ⟨PDLStar⟩ c1
 notation:100 c1"?" => ℋ⟨PDLTest⟩ c1
 notation:100 v:99 "⬝" vs:100 => ℋ⟨consValStack⟩ (v, vs)
-notation:100 "set" "(" mem ", "  x ", "  n ")" => ℋ⟨setMem⟩ (mem, x, n)
-notation:102 "++" x:101 => ℋ⟨PlusPlusVar⟩ x
-notation:102 a1:99 "+" a2:100 => ℋ⟨PlusNat⟩ (a1, a2)
-notation:100 a1:99 "<=" a2:100 => ℋ⟨LEqAExp⟩ (a1, a2)
-
-def plus : SMCForm SortCtrlStack := ℋNom (.ctNom ⟨"plus", by aesop⟩)
-notation:100 "plus" => plus
-
-def leq : SMCForm SortCtrlStack := ℋNom (.ctNom ⟨"leq", by aesop⟩)
-notation:100 "leq" => leq
-
-def skip : SMCForm SortCtrlStack := ℋNom (.ctNom ⟨"skip", by aesop⟩)
-notation:100 "skip" => skip
+notation:100 "set" "(" mem ", "  x ", "  n ")" => ℋ⟨setMem_Var_Val_Mem⟩ (mem, x, n)
+notation:102 "++" x:101 => ℋ⟨«++Var_AExp»⟩ x
+notation:102 a1:99 "+" a2:100 => ℋ⟨«_+_AExp_AExp_AExp»⟩ (a1, a2)
+notation:100 a1:99 "<=" a2:100 => ℋ⟨Leq⟩ (a1, a2)
 
 end SMC
