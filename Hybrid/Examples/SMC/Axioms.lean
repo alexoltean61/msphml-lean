0509a62c1f802e3bd48eb90ac422d16f5cb7e393
Alex Oltean <aoltean@mailbox.org>
2026-02-19 13:02:40 +0200

    misc: Remove redundant comments

--- Hybrid/Examples/SMC/Axioms.lean
+++ Hybrid/Examples/SMC/Axioms.lean
@@ -4,19 +4,16 @@ import Hybrid.Proof.Proofs
 
 namespace SMC
 
--- c(s1 ; s2) ↔ c(s1) ; c(s2)
 def CStmtAx (s1 s2 : SMCForm Stmt)
   : SMCForm CtrlStack :=
     c(s1 ; s2) ←→ c(s1) ; c(s2)
 
--- ⟨vs, mem⟩ → [c(n)] ⟨n ⬝ vs, mem⟩, where n is an integer
 def Aint (vs : SMCForm ValStack)
          (mem : SMCForm Mem)
          (n : SMCForm Nat)
     : SMCForm Config :=
     ⟨vs, mem⟩ ⟶ [c(n)] ⟨n ⬝ vs, mem⟩
 
--- ⟨vs, set(mem, x, n)⟩ → [c(x)] ⟨n ⬝ vs, set(mem, x, n)⟩
 abbrev Aid (vs : SMCForm ValStack)
         (mem : SMCForm Mem)
         (x : SMCForm Var)
@@ -24,7 +21,6 @@ abbrev Aid (vs : SMCForm ValStack)
     : SMCForm Config :=
     ⟨vs, set(mem, x, n)⟩ ⟶ [c(x)] ⟨n ⬝ vs, set(mem, x, n)⟩
 
--- ⟨vs, set(mem, x, n)⟩ → [c(++x)] ⟨(n+1) ⬝ vs, set(mem, x, n+1)⟩
 def App  (vs : SMCForm ValStack)
          (mem : SMCForm Mem)
          (x : SMCForm Var)
@@ -32,46 +28,37 @@ def App  (vs : SMCForm ValStack)
   : SMCForm Config :=
     ⟨vs, set(mem, x, n)⟩ ⟶ [c(++x)] ⟨(n +Nat 1) ⬝ vs, set(mem, x, (n +Nat 1))⟩
 
--- c(a1 + a2) ↔ c(a1) ; c(a2) ; plus
 def DPlus (a1 a2 : SMCForm AExp) : SMCForm CtrlStack :=
     c(a1 + a2) ←→ c(a1) ; c(a2) ; plus
 
--- ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [plus] ⟨n1 + n2 ⬝ vs, mem⟩
 def Aplus (n1 n2 : SMCForm Nat)
           (vs : SMCForm ValStack)
           (mem : SMCForm Mem)
     : SMCForm Config :=
       ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [plus] ⟨(n1 +Nat n2) ⬝ vs, mem⟩
 
--- c(a1 <= a2) ↔ c(a1) ; c(a2) ; leq
 def DLeq (a1 a2 : SMCForm AExp) : SMCForm CtrlStack :=
     c(a1 <= a2) ←→ c(a1) ; c(a2) ; leq
 
--- ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [leq] ⟨↑(n1 ≤ n2), mem⟩
 def ALeq (n1 n2 : SMCForm Nat)
           (vs : SMCForm ValStack)
           (mem : SMCForm Mem)
     : SMCForm Config :=
       ⟨n2 ⬝ n1 ⬝ vs, mem⟩ ⟶ [leq] ⟨(n1 <=Nat n2) ⬝ vs, mem⟩
 
--- γ ⟶ [skip] γ
 def ASkip (γ : SMCForm Config): SMCForm Config :=
       γ ⟶ [c(skip)] γ
 
--- DAsgn c(x := a) ↔ c(a) ; asgn(x)
 def DAsgn (x : SMCForm Var)
           (a : SMCForm AExp) : SMCForm CtrlStack :=
       c(x ::= a) ←→ c(a) ; asgn(x)
 
--- AAsgn ⟨n ⬝ vs, mem) ⟶ [asgn(x)] ⟨vs, set(mem, x, n)⟩
 def AAsgn (n : SMCForm Val)
           (vs : SMCForm ValStack)
           (mem : SMCForm Mem)
           (x : SMCForm Var): SMCForm Config :=
       ⟨n ⬝ vs, mem⟩ ⟶ [asgn(x)] ⟨vs, set(mem, x, n)⟩
 
--- DIf
---   c(if bexp then s1 else s2) ↔ c(bexp) ; ( (true? ; c(s1)) ∪ (false? ; c(s2)) )
 def DIf (bexp : SMCForm BExp)
           (s1 s2 : SMCForm Stmt) : SMCForm CtrlStack :=
       c(bexp) ; ((true : CtNoms Val) ? ; c(s1)) ∪ ((false : CtNoms Val) ? ; c(s2)) ←→ c(if bexp then s1 else s2 endif)
@@ -96,7 +83,7 @@ def ACup (π π' : SMCForm CtrlStack)
 
 def ASeq (π π' : SMCForm CtrlStack)
          (γ : SMCForm Config) : SMCForm Config :=
-        ([π ; π'] γ) ←→ [π][π'] γ  -- FIXME: binding of implications / equivs
+        ([π ; π'] γ) ←→ [π][π'] γ
 
 def ATestTrue (v : SMCForm Val)
          (v' : SMC.nominal Val)
@@ -111,9 +98,6 @@ def ATestFalse (v : SMCForm Val)
          (ψ : SMCForm Config) : SMCForm Config :=
         ℋ@ v' (∼v) ⋀ ⟨v ⬝ vs, mem⟩ ⟶ [v' ?] ψ
 
-
--- DWhile
---   c(while bexp do s) ↔ c(bexp) ; ( (true?) ; c(s) ; c(bexp) )* ; false?
 def DWhile (bexp : SMCForm BExp)
           (s : SMCForm Stmt) : SMCForm CtrlStack :=
     c(while bexp do: s od) ←→ c(bexp) ; (true ? ; c(s) ; c(bexp))* ; false ?
