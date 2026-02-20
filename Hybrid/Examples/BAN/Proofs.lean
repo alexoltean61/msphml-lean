90425f352b8760e5abd25aa1a5228556e63dd234
Bogdan Macovei <bogdan.macovei@essensys.ro>
2026-02-20 02:39:22 +0200

    added NSPK proof in BAN Logic

--- Hybrid/Examples/BAN/Proofs.lean
+++ Hybrid/Examples/BAN/Proofs.lean
@@ -64,15 +64,48 @@ def pf' (i j : BANForm Agent) (m k : BANForm Message)
   (h₁ : BANProof _ (i ◁ ι ⦃m⦄k)) : BANProof _ (i |≡ j |∼ ι m) := by
   exact MMSK h₀ h₁
 
-def i : BANForm Agent := ℋNom "i"
-def j : BANForm Agent := ℋNom "j"
-def k : BANForm Message := ℋNom "k"
-def n : BANForm Message := ℋNom "n"
+namespace SimpleOSS
+  def i : BANForm Agent := ℋNom "i"
+  def j : BANForm Agent := ℋNom "j"
+  def k : BANForm Message := ℋNom "k"
+  def n : BANForm Message := ℋNom "n"
 
-def OSS_proof_sk
-  (h₀ : BANProof _ $ j ◁ ι ⦃n⦄k)
-  (h₁ : BANProof _ $ j |≡ ι shareKeyOp j i k)
-  (h₂ : BANProof _ $ j |≡ #(n))
-  : BANProof _ (j |≡ i |≡ ι n) := by
-  have h₃ := MMSK h₁ h₀
-  exact NV h₃ h₂
+  def OSS_proof_sk
+    (h₀ : BANProof _ $ j ◁ ι ⦃n⦄k)
+    (h₁ : BANProof _ $ j |≡ ι shareKeyOp j i k)
+    (h₂ : BANProof _ $ j |≡ #(n))
+    : BANProof _ (j |≡ i |≡ ι n) := by
+    have h₃ := MMSK h₁ h₀
+    exact NV h₃ h₂
+end SimpleOSS
+
+namespace NSPK
+/-
+  Needham-Schroeder Public-Key (NSPK)
+
+  A -> B : { nA }sk(A)
+  B -> A : { nA, nB }sk(B)
+  A -> B : { nB }sk(A)
+
+  We prove that B |≡ A |≡ nB
+-/
+  def A  : BANForm Agent := ℋNom "A"
+  def B  : BANForm Agent := ℋNom "B"
+  def nA : BANForm Message := ℋNom "nA"
+  def nB : BANForm Message := ℋNom "nB"
+
+def NSPK_mutual_authentication
+  (p₁ : BANProof _ $ B ◁ ι ⦃ nA ⦄sk(A))
+  (p₂ : BANProof _ $ A ◁ ι ⦃ c(nA, nB)⦄sk(B))
+  (p₃ : BANProof _ $ B ◁ ι ⦃ nB ⦄sk(A))
+  (a₁ : BANProof _ $ B |≡ ι pk(A))
+  (a₂ : BANProof _ $ B |≡ #(nB))
+  : BANProof _ (B |≡ (A |≡ ι nB)) := by
+  let h₁ : BANProof _ _ := Proof.ax ⟨ (B |≡ ι pk(A)) ⟶ (B ◁ ι ⦃ nB ⦄sk(A)) ⟶ (B |≡ (A |∼ ι nB)), Nonempty.intro $ Axiom.MMPK⟩
+  let h₂ : BANProof _ $ (B ◁ ι ⦃ nB ⦄sk(A)) ⟶ (B |≡ (A |∼ ι nB)) := Proof.mp h₁ a₁
+  let h₃ : BANProof _ $ B |≡ (A |∼ ι nB) := Proof.mp h₂ p₃
+  let h₄ : BANProof _ $ B |≡ (A |≡ ι nB) := NV h₃ a₂
+  assumption
+
+
+end NSPK
