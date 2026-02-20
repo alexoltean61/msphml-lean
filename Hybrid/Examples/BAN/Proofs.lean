0ab137b6122dd2ec4fea49322293b0417bf5cc8f
Bogdan Macovei <bogdan.macovei@essensys.ro>
2026-02-20 02:26:27 +0200

    completed axiomatization for BAN

--- Hybrid/Examples/BAN/Proofs.lean
+++ Hybrid/Examples/BAN/Proofs.lean
@@ -3,18 +3,40 @@ import Hybrid.Examples.BAN.Signature
 namespace BAN
 
 inductive Axiom : {s : Sorts} → BANForm s → Type
-  | MMSK {i j : BANForm Agent} {k : BANForm Key} {m : BANForm Message}
-    : Axiom $ (i |≡ ι shareKeyOp i j k) ⟶ (i ▸ ι ⦃ m ⦄k) ⟶ (i |≡ j |∼ ι m)
+  | MMSK {i j : BANForm Agent} {m k : BANForm Message}
+    : Axiom $ (i |≡ ι shareKeyOp i j k) ⟶ (i ◁ ι ⦃ m ⦄k) ⟶ (i |≡ j |∼ ι m)
+  | MMPK {i j : BANForm Agent} {m : BANForm Message}
+    : Axiom $ (i |≡ ι pk(j)) ⟶ (i ◁ ι ⦃ m ⦄sk(j)) ⟶ (i |≡ j |∼ ι m)
   | NV {i j : BANForm Agent} {m : BANForm Message}
     : Axiom $ (i |≡ j |∼ ι m) ⟶ (i |≡ #(m)) ⟶ (i |≡ j |≡ ι m)
   | NC {i : BANForm Agent} {m₁ m₂ : BANForm Message}
     : Axiom $ i |≡ #(m₁) ⟶ i |≡ #(c(m₁, m₂))
   | JR {i j : BANForm Agent} {m : BANForm Message}
     : Axiom $ (i |≡ j |≡ ι m) ⟶ (i |≡ j |=> ι m) ⟶ (i |≡ ι m)
+  | BC1₁ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ i |≡ (j |≡ ι c(m₁, m₂)) ⟶ (i |≡ (j |≡ ι m₁))
+  | BC1₂ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ i |≡ (j |≡ ι c(m₁, m₂)) ⟶ (i |≡ (j |≡ ι m₂))
+  | BC2₁ {i : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i |≡ ι c(m₁, m₂)) ⟶ (i |≡ ι m₁)
+  | BC2₂ {i : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i |≡ ι c(m₁, m₂)) ⟶ (i |≡ ι m₂)
   | BC3₁ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
     : Axiom $ (i |≡ j |≡ ι c(m₁, m₂)) ⟶ (i |≡ j |≡ ι m₁)
   | BC3₂ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
     : Axiom $ (i |≡ j |≡ ι c(m₁, m₂)) ⟶ (i |≡ j |≡ ι m₂)
+  | BC4₁ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i |≡ (j |∼ ι c(m₁, m₂))) ⟶ (i |≡ (j |∼ ι m₁))
+  | BC4₂ {i j : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i |≡ (j |∼ ι c(m₁, m₂))) ⟶ (i |≡ (j |∼ ι m₂))
+  | SC1₁ {i : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i ◁ ι c(m₁, m₂)) ⟶ (i ◁ ι m₁)
+  | SC1₂ {i : BANForm Agent} {m₁ m₂ : BANForm Message}
+    : Axiom $ (i ◁ ι c(m₁, m₂)) ⟶ (i ◁ ι m₂)
+  | SC2 {i j : BANForm Agent} {m : BANForm Message}
+    : Axiom $ (i |≡ ι pk(j)) ⟶ (i ◁ ι ⦃m⦄sk(j)) ⟶ (i ◁ ι m)
+  | SC3 {i j : BANForm Agent} {m k : BANForm Message}
+    : Axiom $ (i |≡ ι shareKeyOp i j k) ⟶ (i ◁ ι ⦃m⦄k) ⟶ (i ◁ ι m)
 
 
 @[simp] def BANΛ : AxiomSet BAN := λ _ => { φ | Nonempty (Axiom φ) }
@@ -23,9 +45,9 @@ abbrev BANProof := Proof BANΛ
 
 def MMSK
   (h₁ : BANProof _ (i |≡ ι shareKeyOp i j k))
-  (h₂ : BANProof _ (i ▸ ι ⦃ m ⦄k)) :
+  (h₂ : BANProof _ (i ◁ ι ⦃ m ⦄k)) :
     BANProof _ (i |≡ j |∼ ι m) := by
-  have h₃ : BANProof _ _ := Proof.ax ⟨(i |≡ ι shareKeyOp i j k) ⟶ (i ▸ ι ⦃ m ⦄k) ⟶ (i |≡ j |∼ ι m), Nonempty.intro $ Axiom.MMSK⟩
+  have h₃ : BANProof _ _ := Proof.ax ⟨(i |≡ ι shareKeyOp i j k) ⟶ (i ◁ ι ⦃ m ⦄k) ⟶ (i |≡ j |∼ ι m), Nonempty.intro $ Axiom.MMSK⟩
   have h₄ := Proof.mp h₃ h₁
   exact Proof.mp h₄ h₂
 
@@ -37,21 +59,20 @@ def NV
   have h₄ := Proof.mp h₃ h₁
   exact Proof.mp h₄ h₂
 
-def pf' (i j : BANForm Agent) (k : BANForm Key) (m : BANForm Message)
+def pf' (i j : BANForm Agent) (m k : BANForm Message)
   (h₀ : BANProof _ (i |≡ ι shareKeyOp i j k))
-  (h₁ : BANProof _ (i ▸ ι ⦃m⦄k)) : BANProof _ (i |≡ j |∼ ι m) := by
+  (h₁ : BANProof _ (i ◁ ι ⦃m⦄k)) : BANProof _ (i |≡ j |∼ ι m) := by
   exact MMSK h₀ h₁
 
 def i : BANForm Agent := ℋNom "i"
 def j : BANForm Agent := ℋNom "j"
-def k : BANForm Key := ℋNom "k"
+def k : BANForm Message := ℋNom "k"
 def n : BANForm Message := ℋNom "n"
 
 def OSS_proof_sk
-  (h₀ : BANProof _ $ j ▸ ι ⦃n⦄k)
+  (h₀ : BANProof _ $ j ◁ ι ⦃n⦄k)
   (h₁ : BANProof _ $ j |≡ ι shareKeyOp j i k)
   (h₂ : BANProof _ $ j |≡ #(n))
   : BANProof _ (j |≡ i |≡ ι n) := by
   have h₃ := MMSK h₁ h₀
-  have h₄ := NV h₃ h₂
-  assumption
+  exact NV h₃ h₂
--- Hybrid/Examples/BAN/Signature.lean
+++ Hybrid/Examples/BAN/Signature.lean
@@ -3,49 +3,47 @@ import Hybrid.Language
 import Hybrid.Proof
 
 hybrid_def BAN :=
-  sort Key     ::= builtin String
   sort Agent   ::= builtin String
   sort Message ::= builtin String
-  sort Message ::= "<->"(Agent, Key, Agent) [shareKey]
-  sort Message ::= "enc"(Message, Key)      [encrypt]
-  sort Message ::= "pair"(Message, Message) [pair]
+  sort Message ::= "<->"(Agent, Message, Agent) [shareKey]
+  sort Message ::= "enc"(Message, Message)      [encrypt]
+  sort Message ::= "pair"(Message, Message)     [pair]
+  sort Message ::= "pk"(Agent)                  [pk]
+  sort Message ::= "sk"(Agent)                  [sk]
   sort Nonce   ::= subsort Message
   sort Formula ::= builtin String
-  sort Formula ::= "|≡"(Agent, Formula)     [believes]
-  sort Formula ::= "ι"(Message)             [mtof]
-  sort Formula ::= "|~"(Agent, Formula)     [oncesaid]
-  sort Formula ::= "▸"(Agent, Formula)      [sees]
-  sort Formula ::= "#"(Message)             [nonce]
-  sort Formula ::= "|=>"(Agent, Formula)    [jurisdiction]
+  sort Formula ::= "|≡"(Agent, Formula)         [believes]
+  sort Formula ::= "ι"(Message)                 [mtof]
+  sort Formula ::= "|~"(Agent, Formula)         [oncesaid]
+  sort Formula ::= "◁"(Agent, Formula)          [sees]
+  sort Formula ::= "#"(Message)                 [nonce]
+  sort Formula ::= "|=>"(Agent, Formula)        [jurisdiction]
 
 namespace BAN
 
 @[coe]
 def String.toCtNom (str : String) : BAN.CtNoms BAN.Agent := ⟨str, ⟨str, rfl⟩⟩
 @[coe]
-def String.toCtNom' (str : String) : BAN.CtNoms BAN.Key := ⟨str, ⟨str, rfl⟩⟩
+def String.toCtNom' (str : String) : BAN.CtNoms BAN.Formula := ⟨str, ⟨str, rfl⟩⟩
 @[coe]
-def String.toCtNom'' (str : String) : BAN.CtNoms BAN.Formula := ⟨str, ⟨str, rfl⟩⟩
-@[coe]
-def String.toCtNom''' (str : String) : BAN.CtNoms BAN.Message := ⟨str, ⟨str, rfl⟩⟩
+def String.toCtNom'' (str : String) : BAN.CtNoms BAN.Message := ⟨str, ⟨str, rfl⟩⟩
 
 instance : Coe String (BAN.CtNoms BAN.Agent) where
   coe := String.toCtNom
 
-instance : Coe String (BAN.CtNoms BAN.Key) where
+instance : Coe String (BAN.CtNoms BAN.Formula) where
   coe := String.toCtNom'
 
-instance : Coe String (BAN.CtNoms BAN.Formula) where
-  coe := String.toCtNom''
-
 instance : Coe String (BAN.CtNoms BAN.Message) where
-  coe := String.toCtNom'''
+  coe := String.toCtNom''
 
 def BANForm := Form BAN
 
-def shareKeyOp (a1 a2 : BANForm Agent) (k : BANForm Key) : BANForm Message := ℋ⟨shareKey⟩ (a1, k, a2)
-def encryptOp (m : BANForm Message) (k : BANForm Key) := ℋ⟨encrypt⟩ (m, k)
+def shareKeyOp (a1 a2 : BANForm Agent) (k : BANForm Message) : BANForm Message := ℋ⟨shareKey⟩ (a1, k, a2)
+def encryptOp (m k : BANForm Message) := ℋ⟨encrypt⟩ (m, k)
 def pairOp (m₁ : BANForm Message) (m₂ : BANForm Message) := ℋ⟨pair⟩ (m₁, m₂)
+def pkOp (a : BANForm Agent) := ℋ⟨pk⟩ (a)
+def skOp (a : BANForm Agent) := ℋ⟨sk⟩ (a)
 
 def believeOp (a : BANForm Agent) (φ : BANForm Formula) : BANForm Formula := ℋ⟨believes⟩ (a, φ)
 def mtofOp (m : BANForm Message) : BANForm Formula := ℋ⟨mtof⟩ (m)
@@ -54,12 +52,15 @@ def seesOp (a : BANForm Agent) (φ : BANForm Formula) := ℋ⟨sees⟩ (a, φ)
 def nonceOp (m : BANForm Message) := ℋ⟨nonce⟩ (m)
 def jurisdictionOp (a : BANForm Agent) (φ : BANForm Formula) := ℋ⟨jurisdiction⟩ (a, φ)
 
+notation "pk(" a ")" => pkOp a
+notation "sk(" a ")" => skOp a
+
 notation "⦃" m "⦄" k => encryptOp m k
 notation "c(" m₁ "," m₂ ")" => pairOp m₁ m₂
 
 notation a " |≡ " φ => believeOp a φ
 notation "ι" m => mtofOp m
 notation a " |∼ " φ => oncesaidOp a φ
-notation a " ▸ " φ => seesOp a φ
+notation a " ◁ " φ => seesOp a φ
 notation "#" "(" n ")" => nonceOp n
 notation a " |=> " φ => jurisdictionOp a φ
